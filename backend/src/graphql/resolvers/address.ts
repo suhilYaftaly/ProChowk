d852d383e865442379414e46ac6bdc18344714d3
suhilYaftaly <suhail278@gmail.com>
2023-07-15 13:18:10 -0400

    updates

--- backend/src/graphql/resolvers/address.ts
+++ backend/src/graphql/resolvers/address.ts
@@ -17,38 +17,65 @@ export default {
     ): Promise<[Address]> => {
       const { req } = context;
       const user = checkAuth(req);
-      const mqKey = process.env.MAP_QUEST_KEY;
+
+      const mqResults = await fetchAdrFromMapQuest({ value, lat, lng, limit });
 
       try {
-        const response = await axios.get(
-          `https://www.mapquestapi.com/search/v3/prediction?key=${mqKey}&limit=${limit}&collection=adminArea,poi,address,category,franchise,airport&q=${encodeURIComponent(
-            value
-          )}&location=${lng},${lat}`
-        );
-        const mqResults = response?.data?.results;
         if (mqResults) {
-          return mqResults?.map((res) => {
-            const props = res?.place?.properties;
-            return {
-              id: res.id,
-              displayName: res.displayString,
-              street: props.street,
-              city: props.city,
-              county: props.county,
-              state: props.state,
-              stateCode: props.stateCode,
-              postalCode: props.postalCode,
-              country: props.country,
-              countryCode: props.countryCode,
-              lat: res?.place?.geometry?.coordinates?.[0],
-              lng: res?.place?.geometry?.coordinates?.[1],
-              source: { source: "MapQuest", ...res },
-            };
-          }) as [Address];
-        } else throw gqlError({ msg: "Failed to retrieve data" });
+          return mqResults;
+        } else gqlError({ msg: "Failed to retrieve data" });
       } catch (error: any) {
         throw gqlError({ msg: error?.message });
       }
     },
   },
+};
+
+const fetchAdrFromMapQuest = async ({
+  value,
+  lat,
+  lng,
+  limit,
+}: AddressSearchInput): Promise<[Address]> => {
+  const mqKey = process.env.MAP_QUEST_KEY;
+  const mqSuhilKey = process.env.MAP_QUEST_KEY_SUHIL;
+  const keys = [mqKey, mqSuhilKey];
+
+  const getResponse = async (key: string): Promise<[Address]> => {
+    const resp = await axios.get(
+      `https://www.mapquestapi.com/search/v3/prediction?key=${key}&limit=${limit}&collection=adminArea,poi,address,category,franchise,airport&q=${encodeURIComponent(
+        value
+      )}&location=${lng},${lat}`
+    );
+
+    if (resp?.data?.results) return resp?.data?.results;
+    return undefined;
+  };
+
+  let response;
+  for (let i = 0; i < keys.length; i++) {
+    response = await getResponse(keys[i]);
+    if (response) break;
+  }
+
+  if (response) {
+    return response?.map((res) => {
+      const props = res?.place?.properties;
+      return {
+        id: res.id,
+        displayName: res.displayString,
+        street: props.street,
+        city: props.city,
+        county: props.county,
+        state: props.state,
+        stateCode: props.stateCode,
+        postalCode: props.postalCode,
+        country: props.country,
+        countryCode: props.countryCode,
+        lat: res?.place?.geometry?.coordinates?.[0],
+        lng: res?.place?.geometry?.coordinates?.[1],
+        source: { source: "MapQuest", ...res },
+      };
+    }) as [Address];
+  } else return undefined;
 };
--- web/src/components/reusable/appComps/AddressSearch.tsx
+++ web/src/components/reusable/appComps/AddressSearch.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, ChangeEvent } from "react";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import { Autocomplete, Grid, TextField } from "@mui/material";
+import { Alert, Autocomplete, Grid, TextField } from "@mui/material";
 
 import { getUserLocation } from "@/utils/utilFuncs";
 import { IAddressData, useAddressSearch } from "@gqlOps/address";
@@ -11,7 +11,7 @@ interface Props {
 
 export default function AddressSearch({ onSelect }: Props) {
   const [userCoord, setUserCoord] = useState({ lat: 0, lng: 0 });
-  const { addressSearchAsync, data } = useAddressSearch();
+  const { addressSearchAsync, data, error, loading } = useAddressSearch();
 
   const onGetUserLocation = () => {
     getUserLocation({
@@ -31,42 +31,50 @@ export default function AddressSearch({ onSelect }: Props) {
   };
 
   return (
-    <Autocomplete
-      freeSolo
-      id="free-solo-2-demo"
-      disableClearable
-      getOptionLabel={(option) =>
-        typeof option === "string" ? option : option.displayName
-      }
-      options={data?.addressSearch || []}
-      onChange={(_, value: any) => onSelect(value)}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Address Search"
-          placeholder="23 McSweeney Cres..."
-          onChange={handleInputChange}
-          InputProps={{
-            ...params.InputProps,
-            type: "search",
-            // startAdornment: (
-            //   <InputAdornment position="start">
-            //     <LocationOnIcon />
-            //   </InputAdornment>
-            // ),
-          }}
-        />
-      )}
-      renderOption={(props, option: any) => (
-        <li {...props}>
-          <Grid container alignItems="center" spacing={2}>
-            <Grid item sx={{ display: "flex", gap: 1 }}>
-              <LocationOnIcon sx={{ color: "text.secondary" }} />
-              {option.displayName}
+    <>
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        loading={loading}
+        getOptionLabel={(option) =>
+          typeof option === "string" ? option : option.displayName
+        }
+        options={data?.addressSearch || []}
+        onChange={(_, value: any) => onSelect(value)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Address Search"
+            placeholder="23 McSweeney Cres..."
+            onChange={handleInputChange}
+            InputProps={{
+              ...params.InputProps,
+              type: "search",
+              // startAdornment: (
+              //   <InputAdornment position="start">
+              //     <LocationOnIcon />
+              //   </InputAdornment>
+              // ),
+            }}
+          />
+        )}
+        renderOption={(props, option: any) => (
+          <li {...props}>
+            <Grid container alignItems="center" spacing={2}>
+              <Grid item sx={{ display: "flex", gap: 1 }}>
+                <LocationOnIcon sx={{ color: "text.secondary" }} />
+                {option.displayName}
+              </Grid>
             </Grid>
-          </Grid>
-        </li>
+          </li>
+        )}
+      />
+      {error && (
+        <Alert severity="error" color="error">
+          {error.message}
+        </Alert>
       )}
-    />
+    </>
   );
 }
